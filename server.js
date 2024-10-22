bd1336fcd5c12572d60fddf4151a8be69cf872a1
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-22 23:44:07 +0300

    commit

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/baseController");
+
+
+
 /* ***********************
  * View Engine and Templates
  *************************/
