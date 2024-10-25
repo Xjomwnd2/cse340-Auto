0a8f8ff412cf4795d5448b1ce1d38882abef53a9
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-25 03:02:41 +0300

    COMMIT

--- database/index.js
+++ database/index.js
@@ -1,9 +1,3 @@
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
 const { Pool } = require('pg');
 require("dotenv").config();
 
@@ -13,7 +7,7 @@ if (process.env.NODE_ENV === "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // This allows self-signed certificates for local development.
     },
   });
 
@@ -25,15 +19,27 @@ if (process.env.NODE_ENV === "development") {
         console.log("executed query", { text });
         return res;
       } catch (error) {
-        console.error("error in query", { text });
+        console.error("error in query", { text, error });
         throw error;
-      };
+      }
     },
   };
 
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: true } : false, // This ensures SSL in production if set.
   });
-  module.exports = pool;
+  
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params);
+        return res;
+      } catch (error) {
+        console.error("Production query error", { text, error });
+        throw error;
+      }
+    },
+  };
 };
