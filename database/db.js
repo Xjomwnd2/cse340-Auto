e5a0863e392ec01aafdc07153fb74ad7d52e16b5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-10-26 20:28:12 +0300

    COMMIT

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: '127.0.0.1',
   port: 5432,
-  user: 'cse340',    // Replace with your PostgreSQL username
+  user: 'cse340mn_stxh',    // Replace with your PostgreSQL username
   password: 'https://api.render.com/deploy/srv-csei81lsvqrc73f46uv0?key=zrqNkhKYUYY', // Replace with your PostgreSQL password
   database: 'cse340mn_stxh', // Replace with your database name
   max: 10,                   // Optional: Max number of connections
