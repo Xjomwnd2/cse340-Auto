d82ff95902261d25d866c8cf800ef6e32c07f306
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-11-05 21:08:09 +0300

    commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,17 @@
 const utilities = require("../utilities/");
 const baseController = {};
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+/* ******************************
+ *  Build Home View
+ ****************************** */
+baseController.buildHome = async function (req, res, next) {
+  try {
+    const nav = await utilities.getNav();
+    res.render("index", { title: "Home", nav });
+  } catch (error) {
+    console.error("Error rendering home page:", error);  // Logs the error for debugging
+    next(error);  // Passes the error to Express's error handler
+  }
+};
 
 module.exports = baseController;
