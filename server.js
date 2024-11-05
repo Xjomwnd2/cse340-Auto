a3e63c6f3fe7d9f26407808fcbc209501231753b
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-11-05 21:38:02 +0300

    commit

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 const inventoryRoutes = require('./routes/inventory');
 const homeRoutes = require('./routes/homeRoutes');
-const pool = require('./config/db'); 
 
 const app = express();
 const staticRoutes = require("./routes/static");
