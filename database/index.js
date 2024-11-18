46739055e4e3a57d3d7acd85b735654a61fc6eea
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-11-19 02:16:06 +0300

    commit

--- database/index.js
+++ database/index.js
@@ -1,45 +1,8 @@
 const { Pool } = require('pg');
-require("dotenv").config();
-
-let pool;
-
-if (process.env.NODE_ENV === "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false, // This allows self-signed certificates for local development.
-    },
-  });
-
-  // Added for troubleshooting queries during development
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
-        console.log("executed query", { text });
-        return res;
-      } catch (error) {
-        console.error("error in query", { text, error });
-        throw error;
-      }
-    },
-  };
-
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_SSL ? { rejectUnauthorized: true } : false, // This ensures SSL in production if set.
-  });
-  
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params);
-        return res;
-      } catch (error) {
-        console.error("Production query error", { text, error });
-        throw error;
-      }
-    },
-  };
-};
+const pool = new Pool({
+  host: '127.0.0.1',
+  port: 5432,
+  user: 'yourUsername',
+  password: 'yourPassword',
+  database: 'yourDatabaseName',
+});
