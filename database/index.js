4c8cc09ef31eff0fb857cf530747338264f6034d
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-11-19 02:18:47 +0300

    commit

--- database/index.js
+++ database/index.js
@@ -1,8 +1,16 @@
+// config/db.js
+
 const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
-  host: '127.0.0.1',
-  port: 5432,
-  user: 'yourUsername',
-  password: 'yourPassword',
-  database: 'yourDatabaseName',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'cse340',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  max: 10,
+  idleTimeoutMillis: 30000,
 });
+
+module.exports = pool;
